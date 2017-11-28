886820297b2d2ffb0492175c0aa7aacb44f5ac8d
Anotinas <guslindaaq@student.gu.se>
2017-11-28 20:48:06 +0100

    Further Improvements

--- src/Library.java
+++ src/Library.java
@@ -9,13 +9,14 @@ public class Library {
     private String username = "teamone";
     private String password = "HSaaD5vtp3K6QERq";
 
-    public void login() throws SQLException{
-            Connection conn = DriverManager.getConnection(url, username, password);
-            throw new SQLException("ERROR");
+    public Connection login() throws SQLException {
+        Connection conn;
+        try {
+            conn = DriverManager.getConnection(url, username, password);
+        }catch (Exception E){
+            return null;
+        }
+        return conn;
+        //throw new SQLException("ERROR");
     } // This need to be improved.
-
-
-
-
-
 }
