38b5ab9409cb257d63bf52c03922ace111050dcd
Anotinas <guslindaaq@student.gu.se>
2017-11-28 19:39:54 +0100

    Test

---
+++ src/Library.java
@@ -0,0 +1,10 @@
+
+public class Library {
+
+    private String url = "jdbc:mysql://leia.skip.chalmers.se:3306/team1?autoReconnect=true&useSSL=false";
+    private String username = "teamone";
+    private String password = "HSaaD5vtp3K6QERq";
+
+   
+
+}
