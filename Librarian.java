39bd137281915ad763bbccba5904d55892dcdf7d
Naief <31688507+Naief@users.noreply.github.com>
2017-11-29 13:11:10 +0100

    Partial additions.

---
+++ Librarian.java
@@ -0,0 +1,78 @@
+package package1;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Librarian extends JFrame {
+	static Librarian frame;
+	private JPanel contentPane;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Librarian frame = new Librarian();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Librarian() {
+		setTitle("Library System");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 417, 204);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Librarian login");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setToolTipText("");
+		lblNewLabel.setFont(new Font("Sitka Display", Font.BOLD, 27));
+		lblNewLabel.setBounds(12, 13, 375, 34);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Enter ID");
+		lblNewLabel_1.setFont(new Font("Sitka Display", Font.BOLD, 22));
+		lblNewLabel_1.setBounds(34, 96, 89, 29);
+		contentPane.add(lblNewLabel_1);
+		
+		textField = new JTextField();
+		textField.setBounds(135, 97, 126, 29);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Login");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int id = textField.
+			}
+		});
+		btnNewButton.setFont(new Font("Sitka Display", Font.BOLD, 17));
+		btnNewButton.setBounds(282, 96, 107, 28);
+		contentPane.add(btnNewButton);
+	}
+
+}
