8e39bd09b32caba0b7ea7c8e3f13e9a317628097
Majed <majed-dalain@hotmail.com>
2017-12-10 21:31:48 +0100

    check all added methods!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+    	     // TODO code application logic here
+//        User user1 = new User();
+//        User user2 = new User();
+//        User user3 = new User("700","Gustaf","gustaf","1232","332",0);
+//        //User.addUser(user3);
+//        System.out.println(user1.getTotalFine(6,"Gustaf","700"));
+//        System.out.println(user2.getTotalFine(9,"ahmad","0000"));
+//        System.out.println(Library.getBooksByAuthor("TREVOR NOAH"));
+//        Library.setBookDelayFine(1,1);
+//        Library.setUserTotalFine(1);
+        System.out.println(User.getTotalFine(1, "Majed", "0000"));
+        Library.payTotalFines(1, 30);
+         System.out.println(User.getTotalFine(1, "Majed", "0000"));
+//        
+    }
 
     }
-}
+
