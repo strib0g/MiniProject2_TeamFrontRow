177e42208b1f43015407abaedc32099a779ae5b2
FFrostYY <faticfilip98@gmail.com>
2017-12-10 13:39:56 +0100

    Added login system

--- src/Library.java
+++ src/Library.java
@@ -5,8 +5,10 @@ public class Library {
     private String url = "jdbc:mysql://leia.skip.chalmers.se:3306/team1?autoReconnect=true&useSSL=false";
     private String username = "teamone";
     private String password = "HSaaD5vtp3K6QERq";
+    private static int currentUser;
+    private static int currentLib;
 
-    public Connection login() throws SQLException {
+    public Connection connect() throws SQLException {
         Connection conn;
         try {
             conn = DriverManager.getConnection(url, username, password);
@@ -17,10 +19,90 @@ public class Library {
         //throw new SQLException("ERROR");
     } // This need to be improved.
     
+    public void loginUser(String email, int pin) throws SQLException, Exception
+    {
+    	if(currentUser == 0 && currentLib == 0)
+    	{
+    		Connection conn = this.connect();
+        	String sql = "SELECT `member_id` FROM `member` WHERE `member_email` = ? AND `pin_code` = ?;";
+       		PreparedStatement pStatement = conn.prepareStatement(sql);
+       		pStatement.setString(1, email);
+       		pStatement.setInt(2, pin);
+       		ResultSet result = pStatement.executeQuery(sql);
+       		if(result.first())
+       		{
+       			currentUser = result.getInt(1);
+       		}
+       		else
+       		{
+       			throw new Exception("User not found.");
+       		}
+    	}
+    	else
+    	{
+    		throw new Exception("Someone is already logged in.");
+    	}
+    }
+    
+    public void loginLibrarian(String username, int password) throws SQLException, Exception
+    {
+    	if(currentUser == 0 && currentLib == 0)
+    	{
+    		Connection conn = this.connect();
+        	String sql = "SELECT `librarian_id` FROM `librarian` WHERE `librarian_user_name` = ?"
+        			+ " AND `librarian_password` = ?;";
+       		PreparedStatement pStatement = conn.prepareStatement(sql);
+       		pStatement.setString(1, username);
+       		pStatement.setInt(2, password);
+       		ResultSet result = pStatement.executeQuery(sql);
+       		if(result.first())
+       		{
+       			currentLib = result.getInt(1);
+       		}
+       		else
+       		{
+       			throw new Exception("Librarian not found.");
+       		}
+    	}
+    	else
+    	{
+    		throw new Exception("Someone is already logged in.");
+    	}
+    }
+    
+    public void logout()
+    {
+    	currentUser = 0;
+    	currentLib = 0;
+    }
+    
+    public boolean userLoggedIn()
+    {
+    	if(currentUser != 0)
+    	{
+    		return true;
+    	}
+    	else //this should not ever happen
+    	{
+    		return false;
+    	}
+    }
+    
+    public boolean libLoggedIn()
+    {
+    	if(currentLib != 0)
+    	{
+    		return true;
+    	}
+    	else //this should not ever happen
+    	{
+    		return false;
+    	}
+    }
     public ResultSet getBook(String id) throws SQLException
     {
     	String sql = "SELECT * FROM book WHERE id = " + id;
-    	Statement statement = this.login().createStatement();
+    	Statement statement = this.connect().createStatement();
     	ResultSet result = statement.executeQuery(sql);
     	return result;
     	
@@ -32,9 +114,9 @@ public class Library {
 						String email,
 						String adress) throws SQLException, Exception
     {
-    	if(currentUser) //TODO login system
+    	if(this.libLoggedIn())
     	{
-    		Librarian.addUser(this.login(), pin, name, phone, email, adress);
+    		Librarian.addUser(this.connect(), pin, name, phone, email, adress);
     	}
     	else
     	{
@@ -48,9 +130,9 @@ public class Library {
 						String shelf,
 						String genre) throws SQLException, Exception
     {
-    	if(currentUser) //TODO login system
+    	if(this.libLoggedIn()) 
     	{
-    		Librarian.addBook(this.login(), title, publisher, author, shelf, genre);
+    		Librarian.addBook(this.connect(), title, publisher, author, shelf, genre);
     	}
     	else
     	{
@@ -60,9 +142,9 @@ public class Library {
     
     public void deleteUser(int id) throws SQLException, Exception
     {
-    	if(currentUser) //TODO login system
+    	if(this.libLoggedIn()) 
     	{
-    		Librarian.deleteBook(this.login(), id);
+    		Librarian.deleteBook(this.connect(), id);
     	}
     	else
     	{
@@ -72,9 +154,9 @@ public class Library {
     
     public void deleteBook(int id) throws SQLException, Exception
     {
-    	if(currentUser) //TODO login system
+    	if(this.libLoggedIn()) 
     	{
-    		Librarian.deleteUser(this.login(), id);
+    		Librarian.deleteUser(this.connect(), id);
     	}
     	else
     	{
